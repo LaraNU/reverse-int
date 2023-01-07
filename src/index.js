a6266bbd0e704d6902cf27916c8406cf0a710412
LraNU <kara4516@gmail.com>
2023-01-07 09:55:07 +0300

    feat: creat function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    const numAbs = Math.abs(n)
+	const intArr = Array.from(String(numAbs))
+    const reversed = intArr.reverse().join('')
+    const result = Number(reversed)
+    return result
 }
